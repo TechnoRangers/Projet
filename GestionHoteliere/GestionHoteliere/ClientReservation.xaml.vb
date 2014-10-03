55f803c14205fd766d9805edab372b48ae875c53
Vincent Boucher <Vincentboucher25@outlook.com>
2014-10-03 14:34:23 -0400

    yolo test

--- GestionHoteliere/GestionHoteliere/ClientReservation.xaml.vb
+++ GestionHoteliere/GestionHoteliere/ClientReservation.xaml.vb
@@ -1,9 +1,9 @@
 ﻿Public Class ClientReservation
-    Dim MaBD As P2014_BDTestFrancoisEntities
+    Dim BD As P2014_BDTestFrancoisEntities
 
-    Sub New(ByRef _MaBD As P2014_BDTestFrancoisEntities)
+    Sub New(ByRef _BD As P2014_BDTestFrancoisEntities)
         InitializeComponent()
-        MaBD = _MaBD
+        BD = _BD
     End Sub
 
     Private Sub Cli_BtnContinuer_Click(sender As Object, e As RoutedEventArgs) Handles Cli_BtnContinuer.Click
@@ -16,12 +16,14 @@
 
     Private Sub Cli_BtnRechercher_Click(sender As Object, e As RoutedEventArgs) Handles Cli_BtnRechercher.Click
 
+        Dim RechercheClient_ As New RechercheClient(BD)
+        RechercheClient_.Show()
+        Me.Close()
+
     End Sub
 
     Private Sub Cli_BtnAnnuler_Click(sender As Object, e As RoutedEventArgs) Handles Cli_BtnAnnuler.Click
 
-        Dim Menu_ As New Menu(MaBD)
-        Menu_.Show()
         Me.Close()
 
     End Sub
