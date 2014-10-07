9936435fb256eddb05e50d904365d6d53f2252a7
Vincent Boucher <Vincentboucher25@outlook.com>
2014-10-07 13:43:15 -0400

    commit hier

--- GestionHoteliere/GestionHoteliere/ClientReservation.xaml.vb
+++ GestionHoteliere/GestionHoteliere/ClientReservation.xaml.vb
@@ -4,6 +4,8 @@
     Sub New(ByRef _BD As P2014_BDTestFrancoisEntities)
         InitializeComponent()
         BD = _BD
+
+
     End Sub
 
     Private Sub Cli_BtnContinuer_Click(sender As Object, e As RoutedEventArgs) Handles Cli_BtnContinuer.Click
@@ -16,9 +18,7 @@
 
     Private Sub Cli_BtnRechercher_Click(sender As Object, e As RoutedEventArgs) Handles Cli_BtnRechercher.Click
 
-        'Dim RechercheClient_ As New RechercheClient(BD)
-        'RechercheClient_.Show()
-        'Me.Close()
+        FiltrerDatagrid()
 
     End Sub
 
@@ -27,4 +27,26 @@
         Me.Close()
 
     End Sub
+
+    Sub FiltrerDatagrid()
+
+        Dim resChambre = From rc In BD.tblReservationChambre Join Rcr In BD.tblChambreReservationChambre On rc.NoSeqReservChambre Equals Rcr.NoSeqReservChambre Join Ch In BD.tblChambre On Rcr.NoSeqChambre Equals Ch.NoSeqChambre Join Cl In BD.tblClient On Cl.NoSeqClient Equals rc.NoSeqClient
+        Where Cl.PrenomClient = Res_TextBoxPren.Text And Cl.NomClient = Res_TextBoxNom.Text And Cl.codepostal = Res_TextBoxCodePostal
+        Select Ch.tblTypeChambre, Ch.TypeLit, Ch.NbLit, Ch.EtageChambre, Ch.StatutChambre, Ch.DescChambre
+
+
+
+        If resChambre.toList = Nothing Then
+
+            MessageBox.Show("Le client n'est jamais venu à l'hôtel avant")
+
+        Else
+            Cli_DtgHistorique.ItemsSource = resChambre.ToList
+            Res_TextBoxAdresse.Text = resChambre
+            Res_TxtEmail.Text = resChambre
+            Res_NoTelephone.Text = resChambre
+        End If
+
+    End Sub
 End Class
+
